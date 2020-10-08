e88a9207ecd35b185e9a8bde0f53f36389a762bc
Willy0831 <71075181+Willy0831@users.noreply.github.com>
2020-10-08 19:17:43 +0800

    Update 馬力歐.cpp

--- 755561/馬力歐.cpp
+++ 755561/馬力歐.cpp
@@ -1,20 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int high;
+
     cout << "高度：";
     cin >> high;
 
-    for(int i = 0; i < high+1 ; i++ )
-    {
-        for(int k = 0; k < high-i ; k++)
-        {
+    for(int i = 0 ; i < high+1 ; i++ ) {
+        for(int k = 0 ;k < high-i ;k++) {
             cout << " ";
         }
-        for(int j = 0 ; j < i ; j++ )
-        {
+        for(int j = 0 ; j < i ; j++ ) {
             cout << "#";
         }
         cout << "\n";
