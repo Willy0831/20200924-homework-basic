caebd1f6a0c8352086e22f5b66b581c3daafd1b7
Willy0831 <71075181+Willy0831@users.noreply.github.com>
2020-10-08 19:17:26 +0800

    Update 記帳程式.cpp

--- 755561/記帳程式.cpp
+++ 755561/記帳程式.cpp
@@ -7,10 +7,11 @@ int main() {
     cin >> count;
     total = 0;
 
-    for(int i = 0 ;i < count ;i++){
+    for(int i = 0 ;i < count ;i++) {
         cin >> money;
         total = money + total;
-    }         
+    }
+
     cout << "總合為 :" << total;
     cout << "\n";
 }
